ad22cf404492fceda71b7945d7967812922c4ac6
CarlMathew <moradacarl2711@gmail.com>
2025-02-13 19:40:30 +0800

    updating port

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.get("/home", (req, res) => {
   res.send("Testing server");
 });
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Running in port ${port}`);
 });
